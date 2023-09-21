1ee840f9ff725e01ab2b8610df8bc44bdbdb87cf
Balarangareddy22 <106397346+Balarangareddy22@users.noreply.github.com>
2023-09-21 16:39:09 +0530

    Update app2_1.py

--- app2_1.py
+++ app2_1.py
@@ -7,7 +7,7 @@ import pandas as pd
 from PIL import Image
 
 # Load the saved Random Forest model
-pickle_in = open("C:\\Users\\sathw\\Downloads\\rf_model.pkl",'rb')
+pickle_in = open("C:\ml_app\rf_model.pkl",'rb')
 random_forest_model = pickle.load(pickle_in)
 
 # Define the Streamlit app title
